6f509f9934120d715a1420e6bae5a51ccc00ad58
Joe Chapman <jhchapman@gmail.com>
2021-01-03 15:19:49 -0500

    Updated install

--- macos/dock.sh
+++ macos/dock.sh
@@ -3,14 +3,11 @@
 # Set up the order of dock icons
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Safari.app"
-dockutil --no-restart --add "/Applications/Mail.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
 dockutil --no-restart --add "/Applications/Twitteriffic.app"
 dockutil --no-restart --add "/Applications/Photos.app"
 dockutil --no-restart --add "/Applications/Messages.app"
-dockutil --no-restart --add "/Applications/GitHub Desktop.app"
 dockutil --no-restart --add "/Applications/Atom.app"
-dockutil --no-restart --add "/Applications/MacDown.app"
 dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
